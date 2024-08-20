e6d377b97eff88ebcc50f8e642993b1477846398
Guadalupe Romero <romero.guadalupe.epet20@gmail.com>
2024-08-20 14:27:40 -0300

    Guardando imagen del fondo

--- Sreen/login.jsx
+++ Sreen/login.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, Image } from "react-native";
 
 export default function Login (){
     return(
         <view>
-            <text>Login</text>
+            
+            <view>
+                
+            </view>
+
         </view>
     );
 }
