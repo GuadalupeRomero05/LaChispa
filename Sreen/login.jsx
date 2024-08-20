16a439c0f9b35ae7f315afc4321574c9d9337063
Guadalupe Romero <romero.guadalupe.epet20@gmail.com>
2024-08-20 15:03:44 -0300

    Comentando imagen del fondo

--- Sreen/login.jsx
+++ Sreen/login.jsx
@@ -3,17 +3,22 @@ import { StyleSheet, View, ImageBackground, Image } from 'react-native';
 
 export default function Login() {
   return (
-    <View>
-        <view>
-      <ImageBackground
-        source={require('../assets/fondo.jpg')} style={styles.backgroundImage}>
-      </ImageBackground>
-      </view>
+    <View style={styles.fondo}>
+       {/* <view>
+        <ImageBackground
+            source={require('../assets/fondo.jpg')} style={styles.backgroundImage}>
+        </ImageBackground>
+      </view>*/}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  fondo: {
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  },
   backgroundImage: {
     flex: 1,
     resizeMode: 'cover',
