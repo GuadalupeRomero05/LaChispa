2952bc913bc012af20e02361f9ef357d516d870f
Guadalupe Romero <romero.guadalupe.epet20@gmail.com>
2024-08-20 16:25:56 -0300

    Aplicando estilos a formulario para iniciar sesion

--- Sreen/login.jsx
+++ Sreen/login.jsx
@@ -1,43 +1,85 @@
 import React from 'react';
-import { Text, StyleSheet, View, ImageBackground, Image, TextInput, TouchableOpacity } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
+import { StyleSheet, View, ImageBackground, Image, TextInput, TouchableOpacity } from 'react-native';
+
 
 export default function Login() {
   return (
     <View style={styles.fondo}>
-       {/* <view>
+      {/* Uncomment these if you want a background image */}
+      {/* <view>
         <ImageBackground
-            source={require('../assets/fondo.jpg')} style={styles.backgroundImage}>
+          source={require('../assets/fondo.jpg')}
+          style={styles.backgroundImage}
+        >
         </ImageBackground>
       </view>*/}
-      <view style={styles.formulario}>
-        <view style={styles.cajaTexto}>
-            <TextInput placeholder='correo@gmail.com' style={{paddingHorizontal:15}}/>
-        </view>
+      <View style={styles.formulario}>
+        <View style={styles.cajaTexto}>
+          <TextInput placeholder='correo@gmail.com' style={{ paddingHorizontal: 15 }} />
+        </View>
 
-        <view style={styles.cajaTexto}>
-        <TextInput placeholder='Contraseña' style={{paddingHorizontal:15}}/>
-        </view>
+        <View style={styles.cajaTexto}>
+          <TextInput placeholder='Contraseña' style={{ paddingHorizontal: 15 }} />
+        </View>
+        {/*<view>
+        <TouchableOpacity style={styles.boton}>
+          <Text>Iniciar sesión</Text>
+        </TouchableOpacity>
+        </view>*/}
 
-        <view>
-            <TouchableOpacity>
-                <text>Iniciar sesion</text>
-            </TouchableOpacity>
-        </view>
-      </view>
+        {/* ... */}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   fondo: {
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center'
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   backgroundImage: {
     flex: 1,
     resizeMode: 'cover',
     position: 'absolute',
+  },
+  formulario: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    width: '50%',
+    padding: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  cajaTexto: {
+    flexDirection: 'column',
+    marginBottom: 10,
+    paddingVertical:20,
+    backgroundColor:'#cccccc40',
+    borderRadius:30,
+    marginVertical:10
+
+  },
+  Boton: {
+    alignItems: 'center'
+  },
+  cajaBoton: {
+    backgroundColor: '#525FE1',
+    borderRadius:30,
+    paddingVertical:20,
+    width:150,
+    marginTop:20
+  },
+  textoBoton: {
+    textAlign:'center',
+    color:'white'
   },
 });
