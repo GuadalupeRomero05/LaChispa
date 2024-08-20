95911275df24f44316652c8ccad0eb3784d881a9
Guadalupe Romero <romero.guadalupe.epet20@gmail.com>
2024-08-20 11:25:11 -0300

    Importando handler

--- App.jsx
+++ App.jsx
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// Only import react-native-gesture-handler on native platforms
+import 'react-native-gesture-handler';
 
 export default function App() {
   return (
