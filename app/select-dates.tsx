6aaa45fdced89dedfc087e54e6ee7ccf54cd4a66
Tejada Thais <tejadathaisluciana@gmail.com>
2024-09-26 11:35:14 -0300

    Agregando flecha para volver atras con calendario

---
+++ app/select-dates.tsx
@@ -0,0 +1,67 @@
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import CalendarPicker from 'react-native-calendar-picker';
+import { Colors } from '../constants/Colors';
+import { AntDesign } from '@expo/vector-icons';
+
+export default function Fecha() {
+    const [selectedDate, setSelectedDate] = useState(null);
+
+    // Función para manejar el cambio de fecha
+    const onDateChange = (date) => {
+        setSelectedDate(date); // Guarda la fecha seleccionada en el estado
+        console.log('Fecha seleccionada: ', date); // Puedes manejar la fecha seleccionada aquí
+    };
+
+    return (
+        <View style={styles.mainContainer}>
+           <TouchableOpacity style={styles(isSmallScreen).backButton}onPress={() => NavigationPreloadManager.goBack()}>
+            <AntDesign name="back" size={24} color="black"/>
+            </TouchableOpacity> 
+            <View style={styles.container}>
+                <Text style={styles.header}>Calendario</Text>
+                <CalendarPicker onDateChange={onDateChange} />
+
+                {selectedDate && (
+                    <Text>Fecha seleccionada: {selectedDate.toString()}</Text>
+                )}
+            </View>
+        </View>
+    );
+}
+
+    const styles = (isSmallScreen: boolean) => StyleSheet.create({
+    mainContainer: {
+        padding: 25,
+        paddingTop: 10,
+        backgroundColor: '#f0f8ff',
+        height: '100%',
+    },
+    backButton:{
+        position: "absolute",
+        left: 20, //coloca el boton a la izquierda
+        top: 16,
+        zIndex: 1,
+    },
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: 'f0f8ff',
+        marginTop: 30,
+    },
+    header: {
+        fontSize: 35,
+        fontFamily: 'outfit-Bold', // Asegurarse de que la fuente esté correctamente cargada
+        marginTop: 20,
+    },
+    continueButton: {
+        padding: 20,
+        backgroundColor: Colors.PRIMARY,
+        borderRadius: 15,
+        marginTop: 20,
+    },
+    continueButtonText: {
+        textAlign: 'center',
+        color: Colors.WHITE,
+    },
+});
