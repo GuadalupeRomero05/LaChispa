7bdab055389c5afa0f724c0e95604f5d0c677b35
Thais <tejadathaisluciana@gmail.com>
2024-11-12 19:24:18 -0300

    importando paquetes y biblotecas

--- app/PreceptorInicio.tsx
+++ app/PreceptorInicio.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useContext, useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity, FlatList, StyleSheet, Dimensions } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
@@ -192,16 +191,3 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
 });
-
-=======
-import React from 'react'
-
-function PreceptorInicio() {
-    return (
-        <div>PreceptorInicio</div>
-    )
-}
-
-export default PreceptorInicio
-
->>>>>>> d4626b2489b3344c2b8f02bbf93f1c8e65328955
