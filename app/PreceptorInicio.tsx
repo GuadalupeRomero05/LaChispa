7fccc74a20d53561c3628b969af5728eae902fb7
Thais <tejadathaisluciana@gmail.com>
2024-11-15 00:59:10 -0300

    estilos de la pantalla PreceptorInicio

--- app/PreceptorInicio.tsx
+++ app/PreceptorInicio.tsx
@@ -130,120 +130,6 @@ export default function MyInicio() {
   );
 }
 
-export default function MyInicio() {
-  const navigation = useNavigation<NavigationProp<ParamListBase>>();
-  const [menuVisible, setMenuVisible] = useState(false);
-  const [userMenuVisible, setUserMenuVisible] = useState(false);
-  const { width } = Dimensions.get('window');
-  const isSmallScreen = width < 600;
-
-  const context = useContext(CreateSchoolContext);
-
-  if (!context) {
-    throw new Error("CreateSchoolContext debe ser utilizado dentro de CreateSchoolProvider");
-  }
-
-  const { schoolData, setSchoolData } = context;
-  const [selectedOption, setSelectedOption] = useState<Option | null>(null);
-  const [userEmail, setUserEmail] = useState<string | null>(null);
-
-  useEffect(() => {
-    // Obtener el email del usuario logueado
-    const currentUser = auth.currentUser;
-    if (currentUser) {
-      setUserEmail(currentUser.email);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (selectedOption) {
-      setSchoolData((prevState) => ({
-        ...prevState,
-        year: selectedOption.title,
-      }));
-    }
-  }, [selectedOption]);
-
-  const toggleMenu = () => setMenuVisible(!menuVisible);
-  const toggleUserMenu = () => setUserMenuVisible(!userMenuVisible);
-
-  const handleContinue = () => {
-    navigation.navigate('select-dates');
-  };
-
-
-
-  const handleSignOut = async () => {
-    try {
-      await signOut(auth);
-      console.log('Sesión cerrada');
-      navigation.navigate('index'); // Cambia 'index' por el nombre de la pantalla de inicio de sesión en tu configuración de navegación
-    } catch (error) {
-      console.error('Error al cerrar sesión:', error);
-    }
-  };
-
-  return (
-    <Provider>
-      <View style={styles.container}>
-        {/* Header con íconos alineados */}
-        <View style={styles.header}>
-          <View style={styles.rightIcons}>
-            {/* Menú de configuración */}
-            <Menu
-              visible={menuVisible}
-              onDismiss={toggleMenu}
-              anchor={
-                <TouchableOpacity onPress={toggleMenu} style={styles.iconButton}>
-                  <AntDesign name="setting" size={24} color="black" />
-                </TouchableOpacity>
-              }
-            >
-              
-              <Divider />
-              
-              <Menu.Item onPress={handleSignOut} title="Cerrar sesión" />
-            </Menu>
-
-            {/* Menú de usuario */}
-            <Menu
-              visible={userMenuVisible}
-              onDismiss={toggleUserMenu}
-              anchor={
-                <TouchableOpacity onPress={toggleUserMenu} style={styles.iconButton}>
-                  <AntDesign name="user" size={24} color="black" />
-                </TouchableOpacity>
-              }
-            >
-              <Menu.Item title={userEmail ? `Usuario: ${userEmail}` : 'Cargando usuario...'} />
-            </Menu>
-          </View>
-        </View>
-
-        {/* Título */}
-        <Text style={[styles.title, { fontSize: isSmallScreen ? 24 : 32 }]}>Años a cargo </Text>
-      
-
-        {/* Lista de opciones */}
-        <FlatList
-          data={SelectCursosList}
-          renderItem={({ item }) => (
-            <TouchableOpacity onPress={() => setSelectedOption(item)} style={styles.optionButton}>
-              <OptionCard option={item} selectedOption={selectedOption} />
-            </TouchableOpacity>
-          )}
-          keyExtractor={(item) => item.id.toString()}
-        />
-
-        {/* Botón de continuar */}
-        <TouchableOpacity onPress={handleContinue} style={styles.continueButton}>
-          <Text style={styles.continueButtonText}>Continuar</Text>
-        </TouchableOpacity>
-      </View>
-    </Provider>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -304,3 +190,6 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
 });
+
+
+
